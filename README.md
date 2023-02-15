58340c18c0a54341870ebedce8e746edf92de8af
Victor2760 <89408069+Victor2760@users.noreply.github.com>
2023-02-15 10:53:58 -0800

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,31 @@
+
+> Open this page at [https://victor2760.github.io/treasurehunt/](https://victor2760.github.io/treasurehunt/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/victor2760/treasurehunt** and import
+
+## Edit this project ![Build status badge](https://github.com/victor2760/treasurehunt/workflows/MakeCode/badge.svg)
+
+To edit this repository in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/victor2760/treasurehunt** and click import
+
+## Blocks preview
+
+This image shows the blocks code from the last commit in master.
+This image may take a few minutes to refresh.
+
+![A rendered view of the blocks](https://github.com/victor2760/treasurehunt/raw/master/.github/makecode/blocks.png)
+
+#### Metadata (used for search, rendering)
+
+* for PXT/arcade
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ images.g.ts
@@ -0,0 +1,63 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image2":
+            case "myImage0":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . f f f f . . . . . . 
+. . . . . f e e e e f . . . . . 
+. . . . f f e e e e e f . . . . 
+. . . f e e e e e e e e f . . . 
+. . f e e e e e e e e e e f . . 
+. . f f f f f 5 5 f f f f f . . 
+. . f e e e e 5 5 e e e e f . . 
+. . f e e e e e e e e e e f . . 
+. . f e e e e e e e e e e f . . 
+. . f e e e e e e e e e e f . . 
+. . . f f f f f f f f f f . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image1":
+            case "myImage":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . 1 1 . . . . 
+. . . . . . . 1 1 1 1 1 1 . . . 
+. . . . . . 1 1 1 1 1 1 1 1 . . 
+. . . . 1 1 1 1 f f f f f 1 1 . 
+. . . 1 1 1 1 1 f 1 1 1 1 1 1 . 
+1 1 1 1 1 1 1 1 f 1 1 1 1 1 1 1 
+1 1 1 f f f f f f 1 1 1 1 f f f 
+1 1 1 f 1 1 1 1 1 1 1 1 f f 1 1 
+1 1 1 1 1 1 1 1 1 1 1 f f 1 1 1 
+. 1 1 1 1 1 1 f f f f f 1 1 1 1 
+. . 1 1 1 1 1 f 1 1 1 1 1 1 1 1 
+. . . 1 1 f f 1 1 1 1 1 1 1 1 1 
+. . . 1 1 1 1 1 1 1 1 1 1 1 1 . 
+. . . 1 1 1 1 1 1 1 . . . . . . 
+. . . . 1 1 1 1 . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
